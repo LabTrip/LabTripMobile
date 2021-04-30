463de9b7596fbe70bc59b7ee7c0762dfff87e40e
Guilherme Dantas <guilhermedantas006@gmail.com>
2021-04-30 13:28:09 -0300

    Adicionando botão de chat no orçamento

--- src/pages/listaViagens/detalhesOrcamento.tsx
+++ src/pages/listaViagens/detalhesOrcamento.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import BotaoMais from '../../components/botaoMais';
 import CardOrcamento from '../../components/cardOrcamento';
 import CardDespesasAdicionais from '../../components/cardDespesaAdicional';
 import { useNavigation } from '@react-navigation/native'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
-export default function DetalhesOrcamento() {
+export default function DetalhesOrcamento({route}) {
     const navigation = useNavigation();
+    const rota = route.name;
+    let botaoChat;
+    if (rota == 'Geral') {
+        botaoChat = <TouchableOpacity style={{marginTop:'5%'}} onPress={() => alert('clicou na messagem')}>
+                        <MaterialCommunityIcons name={'chat-processing'} color={'#575757'} size={40} />
+                    </TouchableOpacity>;
+    }
     return (
         <View style={styles.container}>
             <BotaoMais onPress={() =>
                 navigation.navigate('AdicionarDespesa')} />
             <CardOrcamento planejado={30000} saldoAtual={10000} />
-
+            {botaoChat}
             <Text style={styles.label}>Despesas adicionais: </Text>
             <CardDespesasAdicionais data={'12/03/2021'} descricao={'Dogão na praça'} valor={458} />
 
