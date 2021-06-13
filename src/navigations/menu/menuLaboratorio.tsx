016410e24714dc39049c35dda8a5336feb309d8c
Guilherme Dantas <guilhermedantas006@gmail.com>
2021-06-12 21:01:35 -0300

    escondendo aba de cadastro para clientes

--- src/navigations/menu/menuLaboratorio.tsx
+++ src/navigations/menu/menuLaboratorio.tsx
@@ -1,15 +1,48 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import React from 'react'
+import React, { useState } from 'react'
 import ListaEditarViagens from '../../pages/laboratorio/listaEditarViagens';
 import MenuLaboratorioCadastro from './menuLaboratorioCadastro';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Tab = createMaterialTopTabNavigator();
 
 export default function MenuLaboratorio() {
+  const [idPermissao, setIdPermissao] = useState(0);
+
+  const getUsuario = async (idUsuario, token) => {
+    return await fetch('https://labtrip-backend.herokuapp.com/usuarios/' + idUsuario, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'x-access-token': token
+      }
+    });
+  }
+
+  const getIdPermissao = async () => {
+    const token = await AsyncStorage.getItem('AUTH') || "";
+    const idUsuario = await AsyncStorage.getItem('USER_ID') || "";
+    const response = await getUsuario(JSON.parse(idUsuario), JSON.parse(token));
+    const json = await response.json();
+    setIdPermissao(json.perfilId)
+  }
+
+  getIdPermissao();
+
+  let abaCadastros;
+
+  //mostra aba de cadastros se usuario não for um usuario com permissao de cliente.
+  if (idPermissao != 4) {
+    abaCadastros = <Tab.Screen name="Cadastros" component={MenuLaboratorioCadastro} />;
+  }
+
+
   return (
     <Tab.Navigator>
       <Tab.Screen name="Viagens" component={ListaEditarViagens} />
-      <Tab.Screen name="Cadastros" component={MenuLaboratorioCadastro} />
+      {abaCadastros}
     </Tab.Navigator>
   );
+
 }
